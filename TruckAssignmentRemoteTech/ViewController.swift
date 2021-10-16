763c43f155e58e9748477dced31b44f7308d1c57
yogi8886 <yogesh.mangla9@gmail.com>
2021-10-16 19:33:54 +0530

    Initial Commit

---
+++ TruckAssignmentRemoteTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TruckAssignmentRemoteTech
+//
+//  Created by Yogesh on 16/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
